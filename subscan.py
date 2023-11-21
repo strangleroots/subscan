a585b8c01621782cda2e293def6920372aecbe29
strangleroots <151444038+strangleroots@users.noreply.github.com>
2023-11-21 09:19:48 -0500

    Update subscan.py

--- subscan.py
+++ subscan.py
@@ -8,34 +8,34 @@ TDomain = input("what is your target domain?")
 TIP = input("what is your target IP?")
 TFolder = input("what is your desired folder name?")
 
-mkdir = "mkdir +'[TFolder]' ~/bin/go"
+mkdir = "mkdir '{TFolder}' ~/go/bin/"
 subprocess.call(mkdir, shell=True)
 
-wayback1 = "[TDomain] > ~/bin/go/[TFolder]/Targeturl.txt"
+wayback1 = "'{TDomain}' > ~/go/bin/'{TFolder}'/Targeturl.txt"
 subprocess.call(wayback1, shell=True)
 
-wayback2 = "cat /bin/go/[TFolder]/Targeturl.txt | ./waybackurls > ~/bin/go/[TFolder]/target.urls"
+wayback2 = "cat /go/bin/'{TFolder}'/Targeturl.txt | ./waybackurls > ~/go/bin/'{TFolder}'/target.urls"
 subprocess.call(wayback2, shell=True)
 
-wayback3 = "cat /bin/go/[TFolder]/target.urls | httprobe > ~/bin/go/[TFolder]/[TDomain].txt"
+wayback3 = "cat /go/bin'{TFolder}'/target.urls | httprobe > ~/go/bin/'{TFolder}'/'{TDomain}'.txt"
 subprocess.call(wayback3, shell=True)
 
 #targeturl.txt = necessary input for waybacktool
 #target.urls = waybacktool output, to be filtered
 #[TDomain].txt = waybacktool output, filtered, continued on:
 
-sublist3r = "sublist3r -d [TDomain] >> ~/bin/go/[TFolder]/[TDomain].txt" 
+sublist3r = "sublist3r -d '{TDomain}' >> ~/go/bin/'{TFolder}'/'{TDomain}'.txt" 
 subprocess.call(sublist3r, shell=True)
 #append and add
 
-dig = "dig axfr @[TIP] [TDomain] >> ~/bin/go/[TFolder]/[TDomain].txt" 
+dig = "dig axfr @'{TIP}' '{TDomain}' >> ~/go/bin/'{TFolder}'/'{TDomain}'.txt" 
 subprocess.call(dig, shell=True)
 #append and add
 
-amass = "amass enum -passive -d [TDomain] >> ~/bin/go/[TFolder]/[TDomain].txt" 
+amass = "amass enum -passive -d '{TDomain}' >> ~/go/bin/'{TFolder}'/'{TDomain}'.txt" 
 subprocess.call(amass, shell=True)
 #append and add
 
-selection = "cat ~/bin/go/[TFolder]/[TDomain].txt | awk '{print $1}' | sort -u > [TDomain]result.txt" 
+selection = "cat ~/bin/go/'{TFolder}'/'{TDomain}'.txt | awk '{print $1}' | sort -u > '{TDomain}'result.txt" 
 subprocess.call(selection, shell=True)
 #filter into new txt with final result
